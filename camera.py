137b22f63aeb10ccaf196e264323fcaa11968a21
Gabriela Hilario <84292765+gabriela189816@users.noreply.github.com>
2023-10-25 12:46:06 -0600

    probar cámaras por separado

---
+++ camera.py
@@ -0,0 +1,29 @@
+import sys
+import numpy as np
+import cv2 as cv
+
+camera = int(sys.argv[1]) if len(sys.argv) > 1 else 0
+
+cap = cv.VideoCapture(camera)
+cap.set(cv.CAP_PROP_FRAME_WIDTH, 800)
+cap.set(cv.CAP_PROP_FRAME_HEIGHT, 600)
+
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    # Our operations on the frame come here
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    # Display the resulting frame
+    cv.imshow('frame', gray)
+    if cv.waitKey(1) == ord('q'):
+        break
+# When everything done, release the capture
+cap.release()
+cv.destroyAllWindows()
